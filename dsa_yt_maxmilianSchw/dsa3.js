a445b00a8a0737e4fd8023e641c138ca66afd55a
And4Web <anands2807@gmail.com>
2022-05-05 20:35:23 +0530

    sets done

--- dsa_yt_maxmilianSchw/dsa3.js
+++ dsa_yt_maxmilianSchw/dsa3.js
@@ -1,6 +1,26 @@
 /*
 2. Sets
 
+these are also about lists of data but different than arrays.
 
+-> duplication is not allowed.
+-> insertion order is not stored.
+-> element access and extraction via method.
+-> iterable(order is not guaranteed).
+-> size adjusts dynamically.
+-> deletion and finding elements is trivial and faster than arrays.
 
 */
+
+const ids = new Set();
+
+ids.add("kjklsi");
+ids.add(123);
+ids.add("asjdasithaih");
+ids.add(123);
+ids.add(54545767);
+
+console.log(ids);
+console.log(ids.has(123));
+ids.delete(54545767);
+console.log(ids.has(54545767));
